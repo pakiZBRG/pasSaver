e57ac0960b57f6ba2f2ef26dc436e6a3ba2d65c8
Nikola Pavlovic <pavlovicnikola511@gmail.com>
2021-11-28 12:30:34 +0100

    Fixing client folder

---
+++ client/src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Home() {
+    console.log('Evo me')
+    return (
+        <div>
+            <h1>Password Saver</h1>
+            <p>Store all your passwords in one place, and access them using one phrase: keyPass. Get your own keyPass now</p>
+            <input type='email' placeholder='Your email here'/>
+            
+        </div>
+    )
+}
