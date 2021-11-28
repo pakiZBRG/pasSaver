25de6eae57b4cdeed25acb6bf428c864b0383f72
Nikola Pavlovic <pavlovicnikola511@gmail.com>
2021-11-28 13:15:04 +0100

    Style Home Page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,13 +1,32 @@
 import React from 'react';
 
 export default function Home() {
-    console.log('Evo me')
+    const handleForm = (e) => {
+        e.preventDefault();
+        console.log('Poslato')
+    }
+
     return (
-        <div>
-            <h1>Password Saver</h1>
-            <p>Store all your passwords in one place, and access them using one phrase: keyPass. Get your own keyPass now</p>
-            <input type='email' placeholder='Your email here'/>
-            
-        </div>
+        <>
+            <div className='flex-center'>
+                <h1>Password Saver</h1>
+                <p>Store all your passwords in one place, and access them using one phrase: keyPass. Get your own keyPass now</p>
+                <form method='POST' onSubmit={handleForm}>
+                    <input type='email' placeholder='Your email here'/>
+                    <button type='submit'>
+                        <i className='fa fa-arrow-right'></i>
+                    </button>
+                </form>
+            </div>
+            <div className='flex-links'>
+                <a target='_blanc' href='https://github.com/pakiZBRG'>
+                    <i className='fa fa-github'></i>
+                </a>
+                <a target='_blanc' href='https://github.com/pakiZBRG/pasSaver'>
+                    <i className='fa fa-code'></i>
+                </a>
+            </div>
+            <p className='maker'>pakiZBRG</p>
+        </>
     )
 }
